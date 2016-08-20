b9313b7cd0d63329361280135ff7409206271fee
tommy_pm <tommy_pm@hotmail.com>
2016-08-20 16:23:25 -0400

    Styling Ginger of the North text

--- page-splash.php
+++ page-splash.php
@@ -3,16 +3,10 @@
 	<section class="content wrap">
 		<div id="splashscreen" class="orangeback">
 			<div class="logotxt bigpaddingtop bigpaddingbottom">
-				<div class="ginger">
 					<h1>
-						Ginger
+						<span class="ginger">Ginger</span>
+						<span class="north">of the North</span>
 					</h1>
-				</div>
-					<div class="ofthenorth">
-					<h1>
-						of the North
-					</h1>
-				</div>
 			</div>
 			<span>
 				<img src="<?php echo theme_url(); ?>/img/logo.gif">
