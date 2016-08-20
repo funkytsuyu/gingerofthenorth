1a591dfcc2fb3173de387aae17919f3064c4e9f2
tommy_pm <tommy_pm@hotmail.com>
2016-08-20 14:26:41 -0400

    Logo finally fixed. Tried some shit with css.

--- page-splash.php
+++ page-splash.php
@@ -3,13 +3,14 @@
 		<section class="content wrap">
 			<div id="splashscreen" class="orangeback">
 			<div class="logotxt bigpaddingtop bigpaddingbottom">
+				<span>
+				<img src="<?php echo theme_url(); ?>/img/logo.gif">
+				</span>
 				<h2>
 					Ginger of the North
 				</h2>
 			</div>
-			<span>
-				<img src="<?php echo theme_url(); ?>/img/logo.gif">
-			</span>
+		
 		</div>
 		</section>
 
