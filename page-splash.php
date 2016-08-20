59f35d5348867ca8842ab97d2668b720166ac000
tommy_pm <tommy_pm@hotmail.com>
2016-08-20 14:29:26 -0400

    Fixed the css I tried shit on

--- page-splash.php
+++ page-splash.php
@@ -1,17 +1,16 @@
 <?php theme_include('header'); ?>
 
-		<section class="content wrap">
-			<div id="splashscreen" class="orangeback">
+	<section class="content wrap">
+		<div id="splashscreen" class="orangeback">
 			<div class="logotxt bigpaddingtop bigpaddingbottom">
-				<span>
-				<img src="<?php echo theme_url(); ?>/img/logo.gif">
-				</span>
 				<h2>
 					Ginger of the North
 				</h2>
 			</div>
-		
+			<span>
+				<img src="<?php echo theme_url(); ?>/img/logo.gif">
+			</span>
 		</div>
-		</section>
+	</section>
 
 <?php theme_include('footer'); ?>
